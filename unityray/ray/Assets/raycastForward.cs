614154ecbf30a85f7db686bd298f3037e19f256f
ryz <hello@gmail.com>
2016-10-07 20:53:22 +0800

    10/7增加註解

--- unityray/ray/Assets/raycastForward.cs
+++ unityray/ray/Assets/raycastForward.cs
@@ -10,16 +10,17 @@ public class raycastForward : MonoBehaviour {
 		//float theDistance;
 
 		Vector3 forward = transform.TransformDirection (Vector3.forward) * 5;
-
+		//射線長度
 		Debug.DrawRay(transform.position,forward,Color.green);
-
+		//顯示射線
 		if(Physics.Raycast(transform.position,(forward),out hit))
-		{
+		{	//碰到物件顯示
 			//theDistance = hit.distance;
 			//print (theDistance + " 安安你好" + hit.collider.gameObject.name);
 			print("hello");
 		}
 
+		//wsad移動
 		if(Input.GetKey(KeyCode.D))
 		{
 			gameObject.transform.position+=new Vector3(0.5f,0,0);
