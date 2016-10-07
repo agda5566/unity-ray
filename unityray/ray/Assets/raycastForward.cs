bee1b30defefb9d64c26e96e5be6418d487d6ce3
ryz <hello@gmail.com>
2016-10-07 20:38:43 +0800

    raytest10/7

---
+++ unityray/ray/Assets/raycastForward.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raycastForward : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		//float theDistance;
+
+		Vector3 forward = transform.TransformDirection (Vector3.forward) * 5;
+
+		Debug.DrawRay(transform.position,forward,Color.green);
+
+		if(Physics.Raycast(transform.position,(forward),out hit))
+		{
+			//theDistance = hit.distance;
+			//print (theDistance + " 安安你好" + hit.collider.gameObject.name);
+			print("hello");
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			gameObject.transform.position+=new Vector3(0.5f,0,0);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			gameObject.transform.position+=new Vector3(-0.5f,0,0);
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			gameObject.transform.position+=new Vector3(0,0,0.5f);
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			gameObject.transform.position+=new Vector3(0,0,-0.5f);
+		}
+	}
+}
