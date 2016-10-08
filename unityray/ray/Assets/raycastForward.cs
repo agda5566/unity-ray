22c68e4635e49e932162c09fe6eb37df4f653974
ryz <hello@gmail.com>
2016-10-08 19:14:05 +0800

    10/8P.M:7:13減少還沒接觸物體就印出的誤差

--- unityray/ray/Assets/raycastForward.cs
+++ unityray/ray/Assets/raycastForward.cs
@@ -6,14 +6,16 @@ public class raycastForward : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
+		//RaycastHit hit;
 		//float theDistance;
 
-		Vector3 forward = transform.TransformDirection (Vector3.forward) * 5;
+		Vector3 forward = transform.TransformDirection (Vector3.forward);
 		//射線長度
 		Debug.DrawRay(transform.position,forward,Color.green);
 		//顯示射線
-		if(Physics.Raycast(transform.position,(forward),out hit))
+		//10/7會有還沒接觸物體就印出字串的問題
+		//10/8更改以下if最後的數字就能減少誤差
+		if(Physics.Raycast(transform.position, forward, 1))
 		{	//碰到物件顯示
 			//theDistance = hit.distance;
 			//print (theDistance + " 安安你好" + hit.collider.gameObject.name);
@@ -23,19 +25,19 @@ public class raycastForward : MonoBehaviour {
 		//wsad移動
 		if(Input.GetKey(KeyCode.D))
 		{
-			gameObject.transform.position+=new Vector3(0.5f,0,0);
+			gameObject.transform.position+=new Vector3(0.1f,0,0);
 		}
 		if(Input.GetKey(KeyCode.A))
 		{
-			gameObject.transform.position+=new Vector3(-0.5f,0,0);
+			gameObject.transform.position+=new Vector3(-0.1f,0,0);
 		}
 		if(Input.GetKey(KeyCode.W))
 		{
-			gameObject.transform.position+=new Vector3(0,0,0.5f);
+			gameObject.transform.position+=new Vector3(0,0,0.1f);
 		}
 		if(Input.GetKey(KeyCode.S))
 		{
-			gameObject.transform.position+=new Vector3(0,0,-0.5f);
+			gameObject.transform.position+=new Vector3(0,0,-0.1f);
 		}
 	}
 }
