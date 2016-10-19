81b75c2a8945d35d17507ea458ec1d0f39dee8ca
ryz <hello@gmail.com>
2016-10-19 20:50:49 +0800

    10/19test

---
+++ unitycounterparts/counterparts/Assets/testtp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testtp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (transform.gameObject);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(8, 0, 0);
+		print(transform.position.x);
+	}
+	/*void Example() {
+		transform.position = new Vector3(8, 0, 0);
+		print(transform.position.x);
+	}*/
+}
