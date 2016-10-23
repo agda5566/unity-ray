798c0953a54c958c85cef8606a068ca21039cb78
ryz <hello@gmail.com>
2016-10-23 19:48:19 +0800

    bug啊啊啊啊啊啊啊啊啊啊啊啊啊啊

---
+++ raycastmouse/raycastmouse/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	public LayerMask wallLayer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetMouseButton(0) && Physics.Raycast(ray, out hit,wallLayer))
+		{
+			Debug.DrawLine(Camera.main.transform.position, hit.transform.position, Color.red, 0.1f, true);
+			Debug.Log(hit.transform.name);
+		}
+
+	}
+}
