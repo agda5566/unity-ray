49d34157a44d51d2fb95e83fb467cb50ef4a6baf
我想吃雞排 <241092099@nutc.edu.tw>
2016-10-27 21:38:02 +0800

    無言

---
+++ unityhelpmememememememememeQQ/Assets/Timer3.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer3 : MonoBehaviour {
+	public GameObject projectile;
+	public float fireDelta = 0.5F;
+	
+	private float nextFire = 0.5F;
+	private GameObject newProjectile;
+	private float myTime = 0.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1") && myTime > nextFire) {
+			nextFire = myTime + fireDelta;
+			newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+			print("go");
+			// create code here that animates the newProjectile        
+			
+			nextFire = nextFire - myTime;
+			myTime = 0.0F;
+		}
+	}
+}
---
+++ unityhelpmememememememememeQQ/Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	public GameObject projectile;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+			Instantiate(projectile, transform.position, transform.rotation);
+	
+	}
+}
