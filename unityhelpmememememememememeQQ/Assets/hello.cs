7a9a820fc65cb0785ca506613c685436a9e4a6f2
我想吃雞排 <241092099@nutc.edu.tw>
2016-10-27 22:31:56 +0800

    好像略懂

---
+++ unityhelpmememememememememeQQ/Assets/hello.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hello : MonoBehaviour {
+	public float fireDelta = 5.0F;
+
+	private float nextFire = 5.0F;
+	private float myTime = 0.0F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTime = myTime + Time.deltaTime;
+		//float translation = Time.deltaTime;
+		if (Input.GetButton("Fire1") && myTime>nextFire){
+			print ("放開");
+			nextFire = myTime + fireDelta;
+			nextFire = nextFire - myTime;
+			myTime = 0.0F;
+		}
+	}
+}
---
+++ unityhelpmememememememememeQQ/Assets/timepropel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timepropel : MonoBehaviour {
+	private float nextFire = 0.5F;
+	private float myTime = 0.0F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Time.deltaTime * 10;
+		transform.Translate(0, 0, translation);
+	}
+}
